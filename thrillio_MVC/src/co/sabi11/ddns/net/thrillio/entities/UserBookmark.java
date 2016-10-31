efe069232cfb41007d00144cd7a2a02b8c60fe92
SabiKov <b00063874@student.itb.ie>
2016-10-31 00:11:48 +0000

    implemented more entity classes

--- thrillio_MVC/src/co/sabi11/ddns/net/thrillio/entities/UserBookmark.java
+++ thrillio_MVC/src/co/sabi11/ddns/net/thrillio/entities/UserBookmark.java
@@ -6,4 +6,20 @@ public class UserBookmark {
 	private User user;
 	private Bookmark bookmark;
 	
+	//Getter and Setter
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Bookmark getBookmark() {
+		return bookmark;
+	}
+	public void setBookmark(Bookmark bookmark) {
+		this.bookmark = bookmark;
+	}
+	
+	
+	
 }
