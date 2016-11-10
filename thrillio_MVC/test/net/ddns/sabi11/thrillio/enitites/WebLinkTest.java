46366d6c50de1509d13b5ea21bf33e7e5ec7d862
SabiKov <b00063874@student.itb.ie>
2016-11-10 01:40:42 +0000

    fixed typo

--- thrillio_MVC/test/net/ddns/sabi11/thrillio/enitites/WebLinkTest.java
+++ thrillio_MVC/test/net/ddns/sabi11/thrillio/enitites/WebLinkTest.java
@@ -12,35 +12,35 @@ public class WebLinkTest {
 	@Test
 	public void testIsKidsFriendlyEligible() {
 		//Test 1: porn in url -- false
-		WebLink webLink = BookmarkManager.getInstance().creatWebLink(2000, "Taming Tiger", "http://www.javaworld.com/article/2072759/core-java/taming-porn--part-2.html", "http://www.javaworld.com");
+		WebLink webLink = BookmarkManager.getInstance().createWebLink(2000, "Taming Tiger", "http://www.javaworld.com/article/2072759/core-java/taming-porn--part-2.html", "http://www.javaworld.com");
 		
 		boolean isKidFriendlyEligible = webLink.isKidsFriendlyEligible();
 		
 		assertFalse("For porn in url - isKidFriendlyEligible() must return false", isKidFriendlyEligible);
 		
 		//Test 2: porn in title --false
-		webLink = BookmarkManager.getInstance().creatWebLink(2000, "Taming Porn", "http://www.javaworld.com/article/2072759/core-java/taming-part-2.html", "http://www.javaworld.com");
+		webLink = BookmarkManager.getInstance().createWebLink(2000, "Taming Porn", "http://www.javaworld.com/article/2072759/core-java/taming-part-2.html", "http://www.javaworld.com");
 		
 		isKidFriendlyEligible = webLink.isKidsFriendlyEligible();
 		
 		assertFalse("For porn in title - isKidFriendlyEligible() must return false", isKidFriendlyEligible);
 		
 		//Test 3: adult in host -- false
-		webLink = BookmarkManager.getInstance().creatWebLink(2000, "Taming Tiger", "http://www.javaworld.com/article/2072759/core-java/taming-part-tiger-2.html", "http://www.adult.com");
+		webLink = BookmarkManager.getInstance().createWebLink(2000, "Taming Tiger", "http://www.javaworld.com/article/2072759/core-java/taming-part-tiger-2.html", "http://www.adult.com");
 		
 		isKidFriendlyEligible = webLink.isKidsFriendlyEligible();
 		
 		assertFalse("For adult in host - isKidFriendlyEligible() must return false", isKidFriendlyEligible);
 		
 		//Test 4: adult in url, but not in host part -- true
-		webLink = BookmarkManager.getInstance().creatWebLink(2000, "Taming Tiger", "http://www.javaworld.com/article/2072759/core-java/taming-part-adult-2.html", "http://www.javaworld.com");
+		webLink = BookmarkManager.getInstance().createWebLink(2000, "Taming Tiger", "http://www.javaworld.com/article/2072759/core-java/taming-part-adult-2.html", "http://www.javaworld.com");
 		
 		isKidFriendlyEligible = webLink.isKidsFriendlyEligible();
 		
 		assertTrue("For adult in url but not host - isKidFriendlyEligible() must return true", isKidFriendlyEligible);
 		
 		//Test 5 adult in title only -- true
-		webLink = BookmarkManager.getInstance().creatWebLink(2000, "Taming Adult", "http://www.javaworld.com/article/2072759/core-java/taming-part-tiger-2.html", "http://www.javaworld.com");
+		webLink = BookmarkManager.getInstance().createWebLink(2000, "Taming Adult", "http://www.javaworld.com/article/2072759/core-java/taming-part-tiger-2.html", "http://www.javaworld.com");
 		
 		isKidFriendlyEligible = webLink.isKidsFriendlyEligible();
 		
